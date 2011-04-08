033e67b53002649147bc106dc711504840a474ce
Don Wilson <robert.don.wilson@gmail.com>
2011-04-08 08:21:15 -0600

    Improved conveyor tests

--- robots_lasers/test/conveyor_test.rb
+++ robots_lasers/test/conveyor_test.rb
@@ -5,34 +5,52 @@ require 'rdf/conveyor_parser'
 class ConveyorTest < Test::Unit::TestCase
   include RDF
 
-  def test_conveyor_tells_which_way_yields_least_damage
-    conveyor = Conveyor.new
-    conveyor.north_lasers = [nil, nil, nil]
-    conveyor.south_lasers = [nil, nil, 1]
-    conveyor.robot_location = 1
-    
-    assert_equal :west, conveyor.path_of_least_damage
-    
+  def test_conveyor_says_west_if_direction_doesnt_matter
     conveyor = Conveyor.new
     conveyor.north_lasers = [1, 1, 1]
     conveyor.south_lasers = [1, 1, 1]
     conveyor.robot_location = 1
     
     assert_equal :west, conveyor.path_of_least_damage
-    
+
     conveyor = Conveyor.new
-    conveyor.north_lasers = [1, 1, 1]
+    conveyor.north_lasers = [1, nil, 1]
+    conveyor.south_lasers = [1,   1, 1]
+    conveyor.robot_location = 1
+    
+    assert_equal :west, conveyor.path_of_least_damage
+  end
+
+  def test_conveyor_says_west_if_east_is_more_damaging
+    conveyor = Conveyor.new
+    conveyor.north_lasers = [nil, nil, nil]
+    conveyor.south_lasers = [nil, nil,   1]
+    conveyor.robot_location = 1
+    
+    assert_equal :west, conveyor.path_of_least_damage
+
+    conveyor = Conveyor.new
+    conveyor.north_lasers = [nil, nil, nil, nil, nil, nil]
+    conveyor.south_lasers = [nil, nil,   1,   1,   1,   1]
+    conveyor.robot_location = 2
+    
+    assert_equal :west, conveyor.path_of_least_damage
+  end
+
+  def test_conveyor_says_east_if_west_is_more_damaging
+    conveyor = Conveyor.new
+    conveyor.north_lasers = [1, 1,   1]
     conveyor.south_lasers = [1, 1, nil]
     conveyor.robot_location = 1
     
     assert_equal :east, conveyor.path_of_least_damage
-    
+
     conveyor = Conveyor.new
-    conveyor.north_lasers = [1, nil, 1]
-    conveyor.south_lasers = [1, 1, 1]
-    conveyor.robot_location = 1
+    conveyor.north_lasers = [1, 1,   1,   1, 1, nil]
+    conveyor.south_lasers = [1, 1, nil, nil, 1,   1]
+    conveyor.robot_location = 3
     
-    assert_equal :west, conveyor.path_of_least_damage
+    assert_equal :east, conveyor.path_of_least_damage
   end
 
   def test_conveyor_correctly_directs_with_sample_input_file
